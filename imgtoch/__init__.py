01ed867d7dec3265d7c415648f7c69b3a9967b84
hrpzcf <hrpzcf@foxmail.com>
2021-09-17 01:09:50 +0800

    release: 0.0.3

--- imgtoch/__init__.py
+++ imgtoch/__init__.py
@@ -3,7 +3,7 @@
 from .__utils__ import makeImage
 
 NAME = "imgtoch"
-VERSIONNUM = 0, 0, 2
+VERSIONNUM = 0, 0, 3
 VERSION = ".".join(map(str, VERSIONNUM))
 AUTHOR = "hrpzcf"
 EMAIL = "hrpzcf@foxmail.com"
