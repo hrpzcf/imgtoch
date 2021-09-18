828c3a29ad254f30f981339fa430510b703cf272
hrpzcf <hrpzcf@foxmail.com>
2021-09-18 23:43:06 +0800

    feat: 新增两个函数，makeImage函数新增关键字参数chars
    新增用于获取字符名义灰度值的函数grayscaleOf
    新增用于按字符名义灰度值排序的函数sortByGrayscale
    makeImage函数新增关键字参数chars用于自定义字符表

--- imgtoch/__init__.py
+++ imgtoch/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from .__utils__ import makeImage
+from .__utils__ import grayscaleOf, makeImage, sortByGrayscale
 
 NAME = "imgtoch"
 VERSIONNUM = 0, 0, 7
@@ -9,4 +9,4 @@ AUTHOR = "hrpzcf"
 EMAIL = "hrpzcf@foxmail.com"
 WEBSITE = "https://gitee.com/hrpzcf/imgtoch"
 
-__all__ = ["makeImage"]
+__all__ = ["grayscaleOf", "makeImage", "sortByGrayscale"]
